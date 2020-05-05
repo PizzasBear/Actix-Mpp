2e1fb44388f6183fb19a7f51d2ac38bbda89cac6
PizzasBear <mx.sht06@gmail.com>
2020-05-05 11:27:51 +0300

    Postgres in Conf.json

--- src/main.rs
+++ src/main.rs
@@ -40,9 +40,49 @@ async fn index(req: HttpRequest, data: web::Data<AppState<'_>>) -> impl Responde
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    // -- config --
+    let address: String;
+    let is_unix_address: bool;
+    let pg_config: String;
+    match fs::read_to_string("./Conf.json") {
+        Ok(cfg_json) => {
+            let config: serde_json::Value = serde_json::from_str(&cfg_json).unwrap();
+
+            let unix_specified: bool;
+            is_unix_address = match config["unix"].as_bool() {
+                Some(b) => {
+                    unix_specified = true;
+                    b
+                },
+                None => {
+                    unix_specified = false;
+                    false
+                },
+            };
+            
+            address = match config["address"].as_str() {
+                Some(s) => s.to_string(),
+                None => {
+                    if unix_specified {
+                        panic!("If \"unix\" is specified then you have to specify.")
+                    }
+                    else {
+                        "127.0.0.1:8088".to_string()
+                    }
+                },
+            };
+
+            match config["pgConfig"].as_str() {
+                Some(s) => pg_config = s.to_string(),
+                None => panic!("No \"pgConfig\" field found in the file \"Config.json\"."),
+            }
+        },
+        Err(_) => panic!("Failed to read a file."),
+    }
+
     // -- postgres --
     let (client, connection) =
-        match tokio_postgres::connect("", NoTls).await {
+        match tokio_postgres::connect(&pg_config, NoTls).await {
             Ok(a) => a,
             Err(e) => panic!(format!("Couldn't connect to postgres {}", e)),
         };
@@ -62,26 +102,6 @@ async fn main() -> std::io::Result<()> {
         .register_templates_directory(".hbs", "./templates")
         .unwrap();
 
-    // -- config --
-    let mut address = "127.0.0.1:8088".to_string();
-    let mut unix_address = false;
-    match fs::read_to_string("./Conf.json") {
-        Ok(cfg_json) => {
-            let config: serde_json::Value = serde_json::from_str(&cfg_json).unwrap();
-            
-            match config["address"].as_str() {
-                Some(s) => address = s.to_string(),
-                None => { },
-            };
-
-            match config["unix"].as_bool() {
-                Some(b) => unix_address = b,
-                None => { },
-            };
-        },
-        Err(_) => { },
-    }
-
     // -- http server --
     let state_ref = web::Data::new(state);
     let server = HttpServer::new(move || {
@@ -91,7 +111,7 @@ async fn main() -> std::io::Result<()> {
             // .route("/", web::get().to(index))
     });
     
-    if unix_address {
+    if is_unix_address {
         server.bind_uds(address)?
         .run()
         .await
